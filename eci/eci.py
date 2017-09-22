d82de705bf92aebbc96cdd75d015dc6434fd1d06
Zhou <jiaqi.zhou@intel.com>
2017-09-22 10:37:50 +0800

    details

--- eci/eci.py
+++ eci/eci.py
@@ -3,6 +3,7 @@ from load import *
 from core.algorithm import Collective_Influence
 from core.dynamic import reduce_reinert
 import os.path
+import sys
 try:
     import cPickle as pickle
 except ImportError:
@@ -10,7 +11,17 @@ except ImportError:
 
 temp_file_ci_init = '../temp/ci.txt'
 
-g = load_data("../data/test_data.csv")
+if len(sys.argv) != 3:
+    print "python eci.py *.csv clear|noclear"
+    sys.exit()
+
+model_name = sys.argv[1]
+if sys.argv[2] == "clear":
+    print "clear cache file"
+    os.remove(temp_file_ci_init)
+    pass
+
+g = load_data("../data/%s.csv" % model_name )
 if not os.path.isfile(temp_file_ci_init) :
     eci_dic = Collective_Influence(g,2)
     f = open(temp_file_ci_init, 'wb')
@@ -19,6 +30,6 @@ if not os.path.isfile(temp_file_ci_init) :
 eci_dic = pickle.load(open(temp_file_ci_init, "r"))
 
 # eci_dic = Collective_Influence(g,2)
-reduce_reinert(g,eci_dic)
+reduce_reinert(g,eci_dic,"%s.csv.out" % model_name)
 
 # print eci_dic
